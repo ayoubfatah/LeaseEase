71038af06644fffd284f6f1b1dc49e9e76bd0afa
ayoubfatah <ayoubfatah222@gmail.com>
2024-08-02 00:19:54 +0100

    adding the footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import InfoBoxes from "@/components/InfoBoxes";
 
@@ -8,6 +9,7 @@ export default function HomePage() {
     <>
       <HeroSection />
       <InfoBoxes />
+      <Footer />
     </>
   );
 }
---
+++ components/Footer.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Footer() {
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer className="bg-slate-700 text-white py-4 mt-auto">
+      <div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4">
+        <div className="mb-4 md:mb-0">
+          <h1>LeaseEase</h1>
+        </div>
+        <div className="flex flex-wrap justify-center md:justify-start mb-4 md:mb-0">
+          <ul className="flex space-x-4">
+            <li>
+              <Link href="/properties">Properties</Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <p className="text-sm text-gray-100 mt-2 md:mt-0">
+            &copy; {currentYear} LeaseEase. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+}
